9b3eb632034f8149eea11022baa911feeac9af06
VladimirSharldaev <vladimirsarldaev43246@gmail.com>
2022-11-20 21:12:59 +0300

    add exm hard lvl

--- Example3/Program.cs
+++ Example3/Program.cs
@@ -3,37 +3,41 @@
 //  Если да, то вывести всю информацию по нему - площадь, периметр, значения углов треугольника в градусах, является ли он прямоугольным,
 //   равнобедренным, равносторонним.
 
-    Console.WriteLine("Введите сторону треугольника a:");
-    double a = double.Parse(Console.ReadLine());
-    Console.WriteLine("Введите сторону треугольника b:");
-    double b = double.Parse(Console.ReadLine());
-    Console.WriteLine("Введите сторону треугольника c:");
-    double c = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите сторону треугольника a:");
+double a = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите сторону треугольника b:");
+double b = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите сторону треугольника c:");
+double c = double.Parse(Console.ReadLine());
 void checkTriangle(double a, double b, double c)
 {
     if (a + b > c && a + c > b && b + c > a) Console.WriteLine("Треугольник существует");
-    else Console.WriteLine("Треугольник не существует");
-    
+    else
+    {
+        Console.WriteLine("Треугольник не существует");
+    }
 }
 void perimeterAreaTriangle(double a, double b, double c)
 {
+    if (a + b > c && a + c > b && b + c > a){
     double P = a + b + c;
     Console.WriteLine("Периметр = " + P);
     double S = Math.Sqrt(P * (P - a) * (P - b) * (P - c));
-    Console.WriteLine("{0,6:F2}","Площадь = " + S);
+    Console.WriteLine( "Площадь = " + S.ToString("0.##"));}
 }
 
 void anglesTriangle(double a, double b, double c)
 {
-    // cos(β)=(b²+c²−a²)÷(2×b×c); cos(γ)=(a²+c²−b²)÷(2×a×c); cos(α)=(a²+b²−c²)÷(2×a×b). 
-    double A = Math.Acos((a * a + b * b - c * c) / (2 * a * b));
-    double B = Math.Acos((c * c + b * b - a * a) / (2 * c * b));
-    double C = Math.Acos((a * a + c * c - b * b) / (2 * a * c));
-    Console.WriteLine("Угол A=" + A);
-    Console.WriteLine("Угол B=" + B);
-    Console.WriteLine("Угол C=" + C);
-    
+
+    if (a + b > c && a + c > b && b + c > a){
+    double A = (Math.Acos((a * a + b * b - c * c) / (2 * a * b)))*(180.0/Math.PI);
+    double B = (Math.Acos((c * c + b * b - a * a) / (2 * c * b)))*(180.0/Math.PI);
+    double C = (Math.Acos((a * a + c * c - b * b) / (2 * a * c)))*(180.0/Math.PI);
+    Console.WriteLine("Угол A=" + A.ToString("0") + "°");
+    Console.WriteLine("Угол B=" + B.ToString("0") + "°");
+    Console.WriteLine("Угол C=" + C.ToString("0") + "°");
+    }
 }
-checkTriangle(a,b,c);
-perimeterAreaTriangle(a,b,c);
-anglesTriangle(a,b,c);
+checkTriangle(a, b, c);
+perimeterAreaTriangle(a, b, c);
+anglesTriangle(a, b, c);
--- Example4/Program.cs
+++ Example4/Program.cs
@@ -3,3 +3,83 @@
 // Сгенерировать массив случайных целых чисел размерностью m*n (размерность вводим с клавиатуры).
 //  Вывести на экран красивенько таблицей. Найти минимальное число и его индекс, найти максимальное число и его индекс, среднее арифметическое.
 //   Вывести эту информацию на экран.
+int[,] array = CreateArray();
+PrintArray(array);
+Console.WriteLine("Среднее арифмитичкеское " + arifmeticSum(array));
+Console.WriteLine("Наименьшее значение " + minElementArray(array));
+Console.WriteLine("Наибольшое значение " + maxElementArray(array));
+
+int[,] CreateArray()
+{
+    Console.Write("Введите количество строк: ");
+    int m = int.Parse(Console.ReadLine()!);
+    Console.Write("Введите количество столбцов: ");
+    int n = int.Parse(Console.ReadLine()!);
+    int[,] array = new int[m, n];
+
+    Random random = new Random();
+    for (var i = 0; i < m; i++)
+    {
+        for (var j = 0; j < n; j++)
+        {
+            array[i, j] = random.Next(0, 100);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] input)
+{
+    for (var i = 0; i < input.GetLength(0); i++)
+    {
+        for (var j = 0; j < input.GetLength(1); j++)
+        {
+            Console.Write("(" + input[i, j] + ")");
+
+            if (j != input.GetLength(1) - 1)
+                Console.Write(" ");
+            if (input[i, j] < 10) Console.Write(" ");
+
+        }
+        Console.WriteLine();
+    }
+}
+
+int arifmeticSum(int[,] input)
+{
+    int sum = 0;
+    for (var i = 0; i < input.GetLength(0); i++)
+    {
+        for (var j = 0; j < input.GetLength(1); j++)
+        {
+            sum += input[i, j];
+        }
+    }
+    sum = sum / (input.GetLength(0) * input.GetLength(1));
+    return sum;
+}
+
+int minElementArray(int[,] input)
+{
+    int min = 0;
+    for (var i = 0; i < input.GetLength(0); i++)
+    {
+        for (var j = 0; j < input.GetLength(1); j++)
+        {
+            if (min < input[i, j]) min = input[i, j];
+        }
+    }
+    return min;
+}
+int maxElementArray(int[,] input)
+{
+    int max = 0;
+    for (var i = 0; i < input.GetLength(0); i++)
+    {
+        for (var j = 0; j < input.GetLength(1); j++)
+        {
+            if (max > input[i, j]) max = input[i, j];
+        }
+    }
+    return max;
+}
